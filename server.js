c08aee5231bb5e8e6d58d3583d0ffc2774764627
Andrew Kennedy <akennedy2292@gmail.com>
2014-08-01 06:16:34 -0400

    tidied up code

--- server.js
+++ server.js
@@ -1,55 +1,69 @@
 // Load the TCP Library
 net = require('net'),
   config = {
-    host : "192.168.1.131",
-    target : "192.168.3.0/24",
+    host : '192.168.0.120',
+    target : '192.168.0.0/24',
     port : 31337
   },
   clients = [];
+
+
+
+function broadcast(message, sender) {
+  clients.forEach(function (client) {
+    if( client !== sender ){
+      client.write(message);
+    }
+  });
+  console.log(message);
+}
+
+
+function parseMessage( data ){
+  var socket = this,
+    message = data.toString();
+  
+  if( message.match(/^\/name \w+/i) ){
+    var oldName = socket.name;
+    socket.name = escape(message.split('/name ')[1]).split('%')[0]; // my eyes
+    broadcast(oldName + ' is now known as ' + socket.name + '\n');
+  }
+  
+  else if( message.match(/^\/exit/i)){
+    socket.emit('end');
+  }
+
+  else if( message.match(/^\/(commands|help)/i)){
+    socket.write('List of commands:\n/name NAME - change name\n/exit - leave\n');
+  }
+  
+  else {
+    broadcast(socket.name + "> " + data, socket);
+  }
+}
+
+function removeClient(){
+  var socket = this;
+  clients.splice(clients.indexOf(socket), 1);
+  broadcast(socket.name + " left the chat.\n");
+  socket.destroy();
+}
  
-net.createServer(function (socket){
- 
+
+// Create the telnet server
+net.createServer(function( socket ){
+  // Add client
   socket.name = socket.remoteAddress + ':' + socket.remotePort; 
   clients.push(socket);
  
-  // Announce new user
+  // Announce new client
   socket.write('Welcome to the chat!\nYour name is ' + socket.name + '\n');
   broadcast(socket.name + ' joined the chat\n', socket);
  
-  socket.on('data', function (data) {
-    // all of this is so bad
-    if( data.toString().indexOf('/name ') > -1){
-      var oldName = socket.name;
-      socket.name = escape(data.toString().split('/name ')[1]).split('%')[0]; // my eyes
-      broadcast(oldName + ' is now known as ' + socket.name + '\n');
-    }
-    
-    else if( data.toString().indexOf('/exit') > -1){
-      socket.emit('end');
-    }
-    
-    else {
-      broadcast(socket.name + "> " + data, socket);
-    }
-  });
- 
-  socket.on('end', function () {
-    clients.splice(clients.indexOf(socket), 1);
-    broadcast(socket.name + " left the chat.\n");
-  });
-  
-  // Send a message to all clients
-  function broadcast(message, sender) {
-    clients.forEach(function (client) {
-      // Don't want to send it to sender
-      if (client === sender) return;
-      client.write(message);
-    });
-    // Log it to the server output too
-    process.stdout.write(message)
-  }
- 
+  // Bind event listeners
+  socket.on('data', parseMessage);
+  socket.on('end', removeClient);
+
 }).listen(config.port, config.host);
  
-// Put a friendly message on the terminal of the server.
 console.log('Chat server running at port:', config.port);
